e165da07e1fa9d84c88170b37f5e89ac2972bca0
Jairo11711 <116799263+Jairo11711@users.noreply.github.com>
2024-11-18 19:15:47 +0800

    Finished assignment 3 num 2.

---
+++ AlphabetFinder/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main(){
+    string input = "iabbgcqaxyzopbqwpoaigxqaaohdasdfbibcadfgbpwaquglmnopw";
+    // cout<<"String to evaluate: ";
+    // cin>>input;
+
+    string longest_string;
+    string current_longest;
+    int value_to_find = 0;
+
+    for (char& c : input){
+        int ascii_value = int(tolower(c));
+
+        //Check if next token is consecutive
+        if (value_to_find==ascii_value){
+            current_longest += c;
+            value_to_find = ascii_value + 1;
+            
+            if(current_longest.size()>longest_string.size()){
+                longest_string=current_longest;
+            }
+        }
+
+        // Start
+        else if (value_to_find==0){
+            current_longest += c;
+            value_to_find = ascii_value + 1;
+
+            if(current_longest.size()>longest_string.size()){
+                longest_string=current_longest;
+            }
+        }
+
+        //restart token if not consecutive
+        else{
+            value_to_find = 0;
+            current_longest = "";
+        }
+    }
+
+    if (longest_string.size()>1){
+        cout<<"Longest Consecutive Equal Substring Found: "<<longest_string;
+    }  
+    else{
+        cout<<"404 Not Found";        
+    }
+
+
+    return 0;
+}
